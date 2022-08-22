c2807be44d71ac15b101a9d2cdba1b5352a7e854
Ing Zhen Lee <ingzhen2003@gmail.com>
2022-08-22 23:23:46 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+friends = ["Kevin" , "Karen" , "Jim"]
+
+print(friends)
+# to print out the whole list
+
+friends[0] = "Mike"
+# to change the 0 index in the list, Kevin, into Mike
+
+print(friends[1])
+# to print out the 1 index in the list, which is Karen
+
+print(friends[0:])
+# to print out the whole list start from 0 index in the list
+
